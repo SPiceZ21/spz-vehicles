17f0f63f3ed1284d3e652d7d24e97cb88a11f4ef
Yuga Bharathi Jaisankar <yuga.bharathijai2106@gmail.com>
2026-04-12 10:36:14 +0530

    chore: increase spawn and upgrade confirmation timeouts in vehicle config

--- config.lua
+++ config.lua
@@ -5,8 +5,8 @@ Config.SafeZoneCoords          = vector3(0.0, 0.0, 72.0)
 Config.SafeZoneHeading         = 0.0
 
 Config.FreeroamSpawnCooldown   = 10      -- seconds between freeroam spawns
-Config.SpawnConfirmTimeout     = 5000    -- ms to wait for entity spawn confirm
-Config.UpgradeConfirmTimeout   = 3000    -- ms to wait for upgrade confirm
+Config.SpawnConfirmTimeout     = 15000   -- ms to wait for entity spawn confirm
+Config.UpgradeConfirmTimeout   = 10000   -- ms to wait for upgrade confirm
 Config.DespawnDelay            = 500     -- ms between despawn and next spawn
 Config.PollOptionsPerClass     = 2       -- vehicle options per poll
 Config.MaxPlateLength          = 8       -- max chars in /savecustom plate
